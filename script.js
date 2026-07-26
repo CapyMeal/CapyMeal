60f1cf20964d3f29df7b56b07bf96f2b66328a97
MerAtim <guerra.mercedes@hotmail.com>
2026-07-26 02:03:42 -0300

    feat: give CapyMeal its first home

---
+++ script.js
@@ -0,0 +1,11 @@
+const button = document.querySelector(".save-button");
+
+const message = document.querySelector(".message span");
+
+
+button.addEventListener("click", () => {
+
+    message.textContent =
+    "¡Listo! Guardé este momento contigo 🩷";
+
+});
